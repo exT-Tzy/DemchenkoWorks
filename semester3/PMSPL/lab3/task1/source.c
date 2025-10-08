bd95d287b1bf0f54e0f43c4afe0bdc075f8b08dd
exzy <nidemch137@gmail.com>
2025-10-08 14:36:12 +0300

    added lab3 dir in PMSPLS

---
+++ semester3/PMSPL/lab3/task1/source.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int fd[2];
+    pid_t pid;
+    char buf[BUFSIZ];
+    char const* message = "message example 0000_________ooooo";
+    size_t read_bytes, written_bytes;
+
+    if (pipe(fd) == -1)
+    {
+        return EXIT_FAILURE;
+    }
+
+    pid = fork();
+
+    if (pid == -1)
+    {
+        return EXIT_FAILURE;
+    }
+    else if (pid == 0)
+    {
+        close(fd[1]);
+
+        read_bytes = read(fd[0], buf, BUFSIZ - 1);
+        if (read_bytes == -1)
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        buf[read_bytes] = 0;
+        printf("im received: %s\n", buf);
+
+        close(fd[0]);
+    }
+    else
+    {
+        close(fd[0]);
+
+        written_bytes = write(fd[1], message, (strlen(message) + 1) * sizeof(char));
+        if (written_bytes == -1)
+        {
+            exit(EXIT_FAILURE);
+        }
+
+        close(fd[1]);
+
+        wait(NULL);
+    }
+
+    return 0;
+}
